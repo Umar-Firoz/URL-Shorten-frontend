ef4bed8e918c5761407b4ce5b6f718cab8f6a26a
Umar Firoz <f.umar@op.iitg.ac.in>
2026-05-24 14:11:08 +0530

    responsive fix

--- src/components/ui/ShortUrl.jsx
+++ src/components/ui/ShortUrl.jsx
@@ -14,17 +14,17 @@ export function ShortUrl({ newUrl }) {
     }
 
     return (
-        <div className=" flex flex-col gap-4 sm:flex-col md:flex-row  w-full max-w-[650px] items-center justify-between rounded-[18px] border border-[#27272a] bg-[var(--background)] p-4 pl-6 shadow-[0_8px_30px_rgb(0,0,0,0.12)] transition-all duration-290 hover:border-[#3f3f46]">
-            <a href ={newUrl}
+        <div className="flex flex-col gap-4 md:flex-row w-full max-w-[650px] items-center justify-between rounded-[18px] border border-[#27272a] bg-[var(--background)] p-4 md:pl-6 shadow-[0_8px_30px_rgb(0,0,0,0.12)] transition-all duration-290 hover:border-[#3f3f46] overflow-hidden">
+            <a href={newUrl}
                target="_blank"
-               rel="noopener noreferrer" className="cursor-pointer ">
-                <div className="truncate text-[20px] sm:text-[22px] text-center font-medium text-(--text-purple) hover:text-(--text-secondary) tracking-wide mr-4">
+               rel="noopener noreferrer" className="cursor-pointer w-full overflow-hidden flex justify-center md:justify-start">
+                <div className="truncate text-[20px] sm:text-[22px] text-center md:text-left font-medium text-[var(--text-purple)] hover:text-[var(--text-secondary)] tracking-wide md:mr-4 w-full">
                     {newUrl}
                 </div>
             </a>
             
             <button
-                className="flex h-[48px] cursor-pointer items-center justify-center gap-2.5 rounded-[12px] bg-[var(--text-primary)] px-6 text-[16px] font-bold text-[var(--background)] shadow-md transition-all duration-300 hover:scale-105 hover:shadow-lg active:scale-95 flex-shrink-0 cursor-pointer"
+                className="flex w-full md:w-auto h-[48px] cursor-pointer items-center justify-center gap-2.5 rounded-[12px] bg-[var(--text-primary)] px-6 text-[16px] font-bold text-[var(--background)] shadow-md transition-all duration-300 hover:scale-105 hover:shadow-lg active:scale-95 flex-shrink-0"
                 onClick={handleCopy}
             >
                 {!copied ? <Copy size={18} strokeWidth={2.5} /> : <Check size={18} strokeWidth={2.5} />}
