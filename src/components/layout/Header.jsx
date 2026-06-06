6d21ed27753c92bfef0cee70ce60cdfc02ad3495
Umar Firoz <f.umar@op.iitg.ac.in>
2026-06-06 09:55:11 +0530

    reload page fix

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,17 @@
 import { ThemeToggle } from "../ui/ToggleButton.jsx";
 
+
 export function Header() {
     return (
         <div className="fixed z-50 flex h-[72px] w-full items-center justify-between border-b border-[#27272a] bg-[var(--background)] px-8 text-[var(--text-primary)]">
-            <div className=" text-xl font-bold">
+            <div onClick={ ()=> {
+
+                localStorage.removeItem("shortenerUrl");
+                localStorage.removeItem("shortenerResult");
+                localStorage.removeItem("countResult");
+                window.location.reload();
+            }
+            } className=" text-xl font-bold cursor-pointer">
                 {'URL SHORTEN'}
             </div>
             <ThemeToggle />
